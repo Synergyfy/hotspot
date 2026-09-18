5548287811427b21c3489eb887b668b585df9c0e
sundayochuko <sundayochuko101@gmail.com>
2026-09-18 09:26:20 +0100

    fix: JwtAuthGuard respects @Public() decorator, unblock auth and public routes

--- apps/api/src/public/public.controller.ts
+++ apps/api/src/public/public.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CampaignsService } from '../campaigns/campaigns.service';
+import { Public } from '../common/decorators/public.decorator';
 
 @Controller('public')
 export class PublicController {
   constructor(private readonly campaignsService: CampaignsService) {}
 
+  @Public()
   @Get('campaigns/:id')
   findOne(@Param('id') id: string) {
     return this.campaignsService.findOne(+id);
